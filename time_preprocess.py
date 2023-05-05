ce957e6c43c87e2beb815f9379f4fdbdc8836e8b
YenTinaNTHU <tinawang0202@gapp.nthu.edu.tw>
2023-05-05 20:04:12 +0800

    add weather feature

--- time_preprocess.py
+++ time_preprocess.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from config import settings
+import requests
 
 def get_year(date_time:pd.Series, mode='normal'):
     year = date_time.apply(lambda t: t.year)
@@ -81,5 +82,40 @@ def get_is_holiday(date_time:pd.Series):
         return 0
     return date_time.apply(is_holiday)
 
-def get_weather(x, normalized=False, normalize_params=None):
-    return "TODO"
+def get_weather(date_time:pd.Series, mode='normal'):
+    # The longitude and latitude of New York City.
+    lon = '-74.00'
+    lat = '40.71'
+    # get all data
+    start_dt = min(date_time).strftime('%Y-%m-%d')
+    end_dt = max(date_time).strftime('%Y-%m-%d')
+    # call api
+    url = f'https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}&start_date={start_dt}&end_date={end_dt}&hourly=temperature_2m,weathercode&timezone=America%2FNew_York&timeformat=unixtime'
+    response = requests.get(url)
+    if response.status_code == 200:
+        response = response.json()
+        # search the weather by datetime
+        weather_data_df = pd.DataFrame(response["hourly"])
+        weather_data_df['time'] = pd.to_datetime(weather_data_df['time'],unit='s')
+        weather_data_df['time'] = weather_data_df['time'].dt.floor('H')
+        
+        # 將兩個DataFrame的頻率都轉換成小時
+        weather_df = pd.DataFrame({'timestamp': date_time})
+        weather_df['hour'] = weather_df['timestamp'].dt.floor('H')
+        weather_data_df['hour'] =  weather_data_df['time'].dt.floor('H')
+
+        # 合併兩個DataFrame
+        weather_df = pd.merge(weather_df, weather_data_df, on='hour', how='inner').drop('hour', axis=1)
+        weather_df = weather_df.drop(columns=['timestamp', 'time'])
+
+        if mode == 'normalized':
+            scaler = MinMaxScaler()
+            weather_df = pd.DataFrame(scaler.fit_transform(weather_df), columns=weather_df.columns)
+        elif mode == 'standardized':
+            scaler = StandardScaler()
+            weather_df = pd.DataFrame(scaler.fit_transform(weather_df), columns=weather_df.columns)
+        else:
+            ...
+        return weather_df.values
+    else:
+        print("Failed to get weather information.")
